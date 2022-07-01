53a9cafec0d76dca26928b6c488535edb777e2a2
OreOooRanJie <rjlove55@qq.com>
2022-07-02 00:41:36 +0800

    新增: 二分查找以及历史刷题记录

---
+++ 查找/双指针/移除元素.py
@@ -0,0 +1,32 @@
+"""
+给你一个数组 nums和一个值 val，你需要 原地 移除所有数值等于val的元素，并返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+
+元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/remove-element
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        fast, slow = 0, 0
+        while fast < len(nums):
+            if nums[fast] != val:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow
+
+
+if __name__ == "__main__":
+    data = [1, 3, 3, 3, 6]
+    select_sort = Solution()
+    print(select_sort.removeElement(data, 3))
